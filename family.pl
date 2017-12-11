144f96a7858e6902ae2e96243131f66dd49b5f26
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-10 19:18:24 -0800

    added ancestor, grandparent, and second cousin

--- family.pl
+++ family.pl
@@ -14,16 +14,33 @@ brother(X, Y):- male(X), sibling(X, Y).
 
 sister(X, Y):- female(X), sibling(X, Y).
 
+ancestor(X, Y):- parent(X, Y).
+ancestor(X, Y):- parent(X, Z), ancestor(Z, Y).
+
+grandparent(X, Y):- parent(X, Z), parent(Z, Y).
+
+second_cousin(X, Y):- X \= Y, grandparent(Z, X), grandparent(Z, Y), not(sibling(X, Y)).
+
 %
 %  Predicate facts
 %
+
+parent(mike, john).
+parent(mike, jack).
+
+parent(jack, bob).
+parent(ally, bob).
 
 parent(john, mary).
 parent(john, steven).
 parent(sue, mary).
 parent(sue, steven).
 
+male(mike).
 male(john).
 male(steven).
+male(jack).
+male(bob).
+female(ally).
 female(sue).
 female(mary).
