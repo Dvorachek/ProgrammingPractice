118ac61a2c0932e796328e5831d3545e23de07bd
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-11 22:37:15 -0800

    added reverse and palindrome

--- list.pl
+++ list.pl
@@ -5,3 +5,8 @@ concat([Head|Tail], Li, [Head|Rest]):- concat(Tail, Li, Rest).
 
 subset(X, Y):- concat(X, _, Y).
 subset(X, [Y|Ys]):- subset(X, Ys).
+
+reverse([], []).
+reverse([X|Xs], Y):- reverse(Xs, XsY), concat(XsY, [X], Y).
+
+palindrome(X):- reverse(X, Y), Y = X.
