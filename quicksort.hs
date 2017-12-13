08f3f4dc5625c1e4f85b565801a2f82ceaccb80e
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-12 21:11:42 -0800

    added insert

--- quicksort.hs
+++ quicksort.hs
@@ -5,5 +5,7 @@
 quicksort [] = []
 quicksort (p:li) = (quicksort (filter (< p) li)) ++ [p] ++ (quicksort (filter (>= p) li))
 
+insert n l = (filter (< n) l) ++ [n] ++ (filter (>= n) l)
+
 main =
-    print (quicksort [4,3,1,7,8,9,0])
+    print (insert 5 (quicksort [4,3,1,7,8,9,0]))
