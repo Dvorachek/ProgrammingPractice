e408347ca26bb1586f4a33842058a7b8fe5c8605
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-12 19:54:50 -0800

    improved palindrome and sublist; added len, alldif, mergesort and insert

--- list.pl
+++ list.pl
@@ -1,3 +1,5 @@
+% List Operations in Prolog
+
 :-style_check(-singleton).
 
 concat([], Li, Li).
@@ -6,7 +8,30 @@ concat([Head|Tail], Li, [Head|Rest]):- concat(Tail, Li, Rest).
 subset(X, Y):- concat(X, _, Y).
 subset(X, [Y|Ys]):- subset(X, Ys).
 
+% Or in one line
+sublist(S, L):- concat(_, S, M), concat(M, _, L).
+
+member(X, List):- concat(_, [X|_], List).
+
 reverse([], []).
 reverse([X|Xs], Y):- reverse(Xs, XsY), concat(XsY, [X], Y).
 
-palindrome(X):- reverse(X, Y), Y = X.
+palindrome(X):- reverse(X, X).
+
+len([], 0).
+len([_|L], N):- len(L, N1), N is N1 + 1.
+
+alldif([]).
+alldif([_]).
+alldif([X1,X2|Xs]):- dif(X1, X2), alldif([X1|Xs]), alldif([X2|Xs]).
+
+% Mergesort
+merge([], [], []).
+merge(L, [], L).
+merge([], L, L).
+merge([X|Xs], [Y|Ys], [X|L]):- X =< Y, merge(Xs, [Y|Ys], L).
+merge([X|Xs], [Y|Ys], [Y|L]):- X > Y, merge([X|Xs], Ys, L).
+
+insert(X, [], [X]).
+insert(X, [Head|Tail], [Head|Rest]):- X > Head, insert(X, Tail, Rest).
+insert(X, [Head|Tail], [X, Head|Tail]):- X =< Head.
