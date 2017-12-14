08c7fa84ae5e8982cda1fa2d0ee82474828e4bc4
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-14 02:51:52 -0800

    added exception to incorrect command

--- python/contact_list.py
+++ python/contact_list.py
@@ -1,9 +1,9 @@
 # https://www.hackerrank.com/challenges/ctci-contacts/problem
 
-# run python contact_list.py
-# enter "<int>" being the number of commands to be given
-# enter "add <name>" to add a name to the contact list
-# enter "find <keyword>" to search for contacts starting with the keyword
+how_to_use = '''
+enter "<int>" being the number of commands to be given
+enter "add <name>" to add a name to the contact list
+enter "find <keyword>" to search for contacts starting with the keyword'''
 
 
 def partials(contact):
@@ -17,14 +17,17 @@ def add(word):
     for token in partials(word):
         contacts[token] = contacts.get(token, 0) + 1
 
-
+print how_to_use
 n = int(raw_input().strip())
 t = {}
 for a0 in xrange(n):
-    op, contact = raw_input().strip().split(' ')
-    
+    try:
+        op, contact = raw_input().strip().split(' ')
+    except:
+        continue
+
     if op == 'add':
         t = add(contact)
-    if op == 'find':
+    elif op == 'find':
         print find(contact)
 
