6cf18b079b3569fc16165a12d2a4f9418a9c85b0
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-13 21:04:48 -0800

    updated test

--- haskell/quicksort.hs
+++ haskell/quicksort.hs
@@ -8,4 +8,4 @@ quicksort (p:li) = (quicksort (filter (< p) li)) ++ [p] ++ (quicksort (filter (>
 insert n l = (filter (< n) l) ++ [n] ++ (filter (>= n) l)
 
 main =
-    print (insert 5 (quicksort [4,3,1,7,8,9,0]))
+    print (insert 5 (quicksort [4,3,1,7,8,9,0,6,2]))
