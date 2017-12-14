c7c4ee103b76cebcde1974fa7528b7092b167e15
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-13 20:56:06 -0800

    check if tree is a BST

---
+++ python/checkBST.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/ctci-is-binary-search-tree/problem
+
+def checkBST(root):
+    import sys
+    return cBST(root, -sys.maxint, sys.maxint)
+    
+    
+def cBST(root, intmin, intmax):
+    if not root:
+        return True
+    if (root.data <= intmin or root.data >= intmax):
+        return False
+    return (cBST(root.left, intmin, root.data) and cBST(root.right, root.data, intmax))
+    
+    
