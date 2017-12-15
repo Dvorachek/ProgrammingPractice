98b235cb7c884495ec04827abd6b3cccd59e79a0
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-14 19:04:47 -0800

    added associative to the left reduce

--- haskell/map_reduce_filter.hs
+++ haskell/map_reduce_filter.hs
@@ -6,8 +6,13 @@
 map1 f [] = []
 map1 f (x:xs) = (f x) : (map1 f xs)
 
+-- associative to the right
 reduce1 f [] u = u
 reduce1 f (x:xs) u = f x (reduce1 f xs u)
+
+-- associative to the left
+reduce2 f [] u = u
+reduce2 f (x:xs) u = reduce2 f xs (f x u)
 
 filter1 p [] = []
 filter1 p (x:xs) =
@@ -26,6 +31,9 @@ power n f = f . (power (n-1) f)
 main = do
     -- sum the squares of a list
     print (reduce1 (+) (map1 (\n -> n * n) [1..10]) 0)
+    
+    -- sum a list using left associative reduce
+    print (reduce2 (+) [1..10] 0)
 
     -- list of squares from 1..100 divisible by 7
     print (filter1 (\n -> n `mod` 7 == 0) (map1 (\n -> n * n) [1..100]))
