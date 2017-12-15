66aaf9e0d231970bd985206a1846bb91f2739d3a
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-14 21:35:37 -0800

    added 3 by 3 latin square problem.. need to split into new file when I have time

--- prolog/list.pl
+++ prolog/list.pl
@@ -44,3 +44,41 @@ enclose(S, L):- concat(S, _, M), concat(M, S, L).
 sort2([], []).
 sort2(L, L).
 sort2([X|L1], L2):- insert(X, L, L2), sort2(L1, L).
+
+repeated(L):- concat(Ls, Ls, L).
+
+adjacent(U, V, L):- concat(_, [U, V|_], L); concat(_, [V, U|_], L).
+
+latin(X1, X2, X3, X4, X5, X6, X7, X8, X9):- 
+    X1 \= X2,
+    X1 \= X3,
+    X1 \= X4,
+    X1 \= X7,
+    X2 \= X3,
+    X2 \= X5,
+    X2 \= X8,
+    X3 \= X6,
+    X3 \= X9,
+    X4 \= X7,
+    X4 \= X5,
+    X4 \= X6,
+    X5 \= X8,
+    X5 \= X6,
+    X6 \= X9,
+    X7 \= X8,
+    X7 \= X9,
+    X8 \= X9,
+    num(X1),
+    num(X2),
+    num(X3),
+    num(X4),
+    num(X5),
+    num(X6),
+    num(X7),
+    num(X8),
+    num(X9).
+
+
+num(1).
+num(2).
+num(3).
