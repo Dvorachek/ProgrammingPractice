7d50c6cd5a26e51b6e92fe083852f7c50c13a966
Dylan <Dylan.Dvorachek@gmail.com>
2017-12-14 16:39:14 -0800

    added filter

--- haskell/map_reduce.hs
+++ haskell/map_reduce.hs
@@ -9,5 +9,15 @@ map1 f (x:xs) = (f x) : (map1 f xs)
 reduce1 f [] u = u
 reduce1 f (x:xs) u = f x (reduce1 f xs u)
 
--- sum the squares of a list
-main = print (reduce1 (+) (map1 (^2) [1..10]) 0)
+filter1 p [] = []
+filter1 p (x:xs) =
+    if (p x)
+    then x : (filter1 p xs)
+    else filter1 p xs
+
+main = do
+    -- sum the squares of a list
+    print (reduce1 (+) (map1 (\n -> n * n) [1..10]) 0)
+
+    -- list of squares from 1..100 divisible by 7
+    print (filter1 (\n -> n `mod` 7 == 0) (map1 (\n -> n * n) [1..100]))
